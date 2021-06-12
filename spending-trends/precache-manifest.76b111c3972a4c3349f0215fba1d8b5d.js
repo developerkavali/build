6d8842a730eaf59cb5bc96332ce6e68fa3275ff0
developer.kavali <kavalimayi@gmail.com>
2021-06-12 19:59:20 +0530

    spending-trends build

---
+++ spending-trends/precache-manifest.76b111c3972a4c3349f0215fba1d8b5d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b81cb78f2f591c78bffd89b3cceb60d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5b20855119588dbafad",
+    "url": "/static/css/2.54c80412.chunk.css"
+  },
+  {
+    "revision": "3366cf56d5951710254a",
+    "url": "/static/css/main.697df292.chunk.css"
+  },
+  {
+    "revision": "a5b20855119588dbafad",
+    "url": "/static/js/2.c904af39.chunk.js"
+  },
+  {
+    "revision": "4286d4a2dbb0ddcf5adf90c5918d90f5",
+    "url": "/static/js/2.c904af39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3366cf56d5951710254a",
+    "url": "/static/js/main.81478f1b.chunk.js"
+  },
+  {
+    "revision": "2db95ead441e081cdccd",
+    "url": "/static/js/runtime-main.3dcb73a2.js"
+  }
+]);
